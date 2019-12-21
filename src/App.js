7426d2d04086d96457759f0e9ac7f47e02c97a54
Kallirroi <kallirroi.retzepi@gmail.com>
2019-12-21 20:11:52 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactBotUI from './ReactBotUI';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactBotUI
+          dialogHeightMax={350}
+          isUserHidden={false}
+          isVisible={true} 
+          title={'Testing out Dialogflow'} />
     </div>
   );
 }
