78db1f9d688d6b37f8554036859f8284957e8847
agnescameron <agnesfcameron@gmail.com>
2020-01-22 19:11:08 -0500

    never mind lol

--- src/lambda/botRequest.js
+++ src/lambda/botRequest.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import dialogflow from 'dialogflow';
 const uuid = require('uuid');
-let truthbotPrivateKey = (process.env.truthbot_private_key);
+let truthbotPrivateKey = JSON.parse(process.env.truthbot_private_key).private_key;
 let truthbotClientEmail = process.env.truthbot_client_email;
 let truthbotProjectId = process.env.truthbot_project_id;
 
