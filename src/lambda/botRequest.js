7fa892e0a501bef49e9cf0ba25729337462bb523
agnescameron <agnesfcameron@gmail.com>
2020-01-28 11:04:53 -0500

    storing bot as JSON file

--- src/lambda/botRequest.js
+++ src/lambda/botRequest.js
@@ -1,9 +1,7 @@
 import fetch from 'node-fetch';
 import dialogflow from 'dialogflow';
 const uuid = require('uuid');
-let truthbotPrivateKey = process.env.truthbot_private_key.replace(/\\n/g, '\n');
-let truthbotClientEmail = process.env.truthbot_client_email;
-let truthbotProjectId = process.env.truthbot_project_id;
+let botJSON = JSON.parse(process.env.bots_json).bots[0];
 
 exports.handler = async (event) => {
     // "event" has information about the path, body, headers, etc. of the request
@@ -16,21 +14,19 @@ exports.handler = async (event) => {
   }
 }
 
-
 const sessionId = uuid.v4();
+console.log('session id is', sessionId)
 
 // Create a new session
 const sessionClient = new dialogflow.SessionsClient({
-    projectId: truthbotProjectId,
+    projectId: botJSON.project_id,
     credentials: {
-      private_key: truthbotPrivateKey,
-      client_email: truthbotClientEmail
+      private_key: botJSON.private_key,
+      client_email: botJSON.client_email
     }
 });
 
-const sessionPath = sessionClient.sessionPath(truthbotProjectId, sessionId);
-console.log('sessionpath is', sessionClient);
-
+const sessionPath = sessionClient.sessionPath(botJSON.project_id, sessionId);
 
 async function botRequest(userString) {
     // The text query request.
