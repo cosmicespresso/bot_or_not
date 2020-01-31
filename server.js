70e89a80c38e8f9e77f17dc7a61f2f6e016375bc
agnescameron <agnesfcameron@gmail.com>
2020-01-31 12:11:21 -0500

    added ngrok

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const proxy = require('http-proxy-middleware')
 const Bundler = require('parcel-bundler')
 const express = require('express')
+const { WebhookClient } = require('dialogflow-fulfillment');
 
 const bundler = new Bundler('src/site/index.html', {
   cache: false
@@ -17,6 +18,28 @@ app.use(
     }
   })
 )
+
+app.post('/', express.json(), (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res })
+
+function getWishFood() {
+    //if contains emoji
+    console.log('called getwishfood')
+    var regex = /([\uD800-\uDBFF][\uDC00-\uDFFF])/g
+    var emoji = agent.request_.body.queryResult.queryText.match(regex);
+
+    console.log(agent.request_.body.queryResult.parameters, emoji)
+
+    agent.add(agent.request_.body.queryResult.fulfillmentText);
+  }
+
+//maps an intent (in this case, wish food) 
+//to a function that gets triggered when an intent is called
+let intentMap = new Map();
+  intentMap.set('Wish Food', getWishFood);
+  agent.handleRequest(intentMap);
+})
+
 
 app.use(bundler.middleware())
 
