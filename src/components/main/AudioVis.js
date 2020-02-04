c93d9aa665098d82c94bc6e516320f9aaebd1b92
Kallirroi <kallirroi.retzepi@gmail.com>
2020-02-04 11:04:56 -0500

    forked react-mic doesnt have the continuous recording glitch

--- src/components/main/AudioVis.js
+++ src/components/main/AudioVis.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { ReactMic } from 'react-mic';
+import { ReactMic } from '@cleandersonlobo/react-mic';
 
 class AudioVis extends Component {
 	
@@ -9,35 +9,40 @@ class AudioVis extends Component {
 	      record: false
 	    }
 	}
-	
+
 	startRecording = () => {
-		console.log('started recording')
 		this.setState({ record: true });
-	}
+	}	
 
 	stopRecording = () => {
 		this.setState({ record: false });
-	}
-
-	onData(recordedBlob) {
-		console.log('chunk of real-time data is: ', recordedBlob);
 	}
 
 	onStop(recordedBlob) {
 		console.log('recordedBlob is: ', recordedBlob);
 	}
 
+	componentDidMount() {
+		console.log('should start');
+		this.startRecording();
+	}
+
+	componentWillUnmount() {
+		console.log('should stop');
+	}
+
 	render(props) {
 		return (
 			<div className="narrator" style={{height: `${this.props.dialogHeight}px`}}>
 				<ReactMic
-		          record={true}
-		          visualSetting="frequencyBars"
-		          className="sound-wave"
+		          record={this.state.record}
 		          onStop={this.onStop}
-		          onData={this.onData}
+		          visualSetting='frequencyBars'
+		          className="sound-wave"
 		          strokeColor="#FF2D55"
-		          backgroundColor="#fff" />
+		          backgroundColor="#fff"
+		          />
+		          <button onClick={this.stopRecording} type="button">Stop</button>
 			</div>
 		);
 	}
