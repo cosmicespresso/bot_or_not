ae297240a58cbbe1419f0647e6e763990ebb3779
Kallirroi <kallirroi.retzepi@gmail.com>
2020-02-11 08:56:58 -0500

    back to older module

--- src/components/main/AudioVis.js
+++ src/components/main/AudioVis.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import AudioRecorder from 'react-audio-recorder';
+import { ReactMic } from '@cleandersonlobo/react-mic';
+
 
 class AudioVis extends Component {
 	
@@ -20,7 +21,7 @@ class AudioVis extends Component {
 
 	componentDidMount() {
 		console.log('AudioVis mounted, starting recording');
-		// this.startRecording();
+		this.startRecording();
 	}
 
 	componentWillUnmount() {
@@ -30,7 +31,18 @@ class AudioVis extends Component {
 	render(props) {
 		return (
 			<div className="narrator" style={{height: `${this.props.dialogHeight}px`}}>
-				hey
+				<ReactMic
+		          record={this.state.record}
+		          onStop={this.onStop}
+		          visualSetting='frequencyBars'
+		          className="sound-wave"
+		          strokeColor="#FF2D55"
+		          backgroundColor="#fff"
+		          className="field-top"
+		          />
+		          <div onClick={this.stopRecording} className='AudioVis-button'>
+					<p>Stop recording</p>
+				  </div>
 			</div>
 		);
 	}
