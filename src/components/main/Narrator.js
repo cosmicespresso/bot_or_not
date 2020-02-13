787a55ae506aa007d45df09dc0ceba3be4878866
Kallirroi <kallirroi.retzepi@gmail.com>
2020-02-13 14:24:20 -0500

    added dangerouslysetinnerHTML for Narrator fieldBottom var >_<

--- src/components/main/Narrator.js
+++ src/components/main/Narrator.js
@@ -4,7 +4,7 @@ function Narrator(props)  {
 	return (
 		<div className="narrator" style={{height: `${props.dialogHeight}px`}}>
 			<div className="field-top">{props.headline}</div>
-			<div className="field-bottom">{props.text}</div>
+			<div className="field-bottom"  dangerouslySetInnerHTML={{__html: props.text}} />
 		</div>
 	);
 }
