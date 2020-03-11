aed0a942c3812e97bdbefb6dde46b2861473898f
Kallirroi <kallirroi.retzepi@gmail.com>
2020-03-11 10:16:53 -0400

    removed audio components

--- src/components/main/Chat.js
+++ src/components/main/Chat.js
@@ -1,47 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Exchange from './Exchange';
-import { ReactMic } from 'react-mic';
 
 class Chat extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			record: false
-		}
-	}
 
 	scrollToBottom = () => {
 		const end = ReactDOM.findDOMNode(this.scrollTarget);
 		end.scrollIntoViewIfNeeded({behavior: 'smooth'});
-	}
-
-	startRecording = () => {
-		console.log('start recording')
-    	this.setState({
-      		record: true
-		});
-	}
-
-	stopRecording = () => {
-		console.log('stop recording')
-		this.setState({
-	  		record: false
-		});
-	}
-
-	onStop(recordedBlob) {
-		console.log('recordedBlob is: ', recordedBlob);
-	}
-
-	componentDidMount() {
-		if (this.props.choice === 'Dare') {
-			setTimeout( () => { this.startRecording() }, 4000);
-		}
-	}
-
-	componentWillUnmount() {
-		this.setState({ record: false });
 	}
 
 	render(props) {
@@ -67,38 +32,17 @@ class Chat extends Component {
 			}
 		}
 
-		let displayChat = !this.state.record ? 'flex' :'none';
-		let displayAudioVis = this.state.record ? 'flex' : 'none';
-
 		return (
-			<div> 
-				{/* ---- Chat ---- */}
-				<div style={{display: displayChat , height: `${this.props.dialogHeight}px`}} 
-					className="messages-wrapper" >
-					<div className="messages">
-						{groups.map((group, i) =>
-							<Exchange key={i} group={group}/>
-						)}
-						<div style={{ float: "left", clear: "both" }} ref={el => this.scrollTarget = el} > </div>
-					</div>
-				</div>
-	            
-				{/* ---- AudioVis ---- */}
-	            <div className="narrator" style={{display: displayAudioVis, height: `${this.props.dialogHeight}px`}}>
-					<ReactMic
-			          record={this.state.record}
-			          onStop={this.onStop}
-			          onData={this.onData}
-			          visualSetting='frequencyBars'
-			          className="sound-wave"
-			          strokeColor="#FF2D55"
-			          backgroundColor="#fff"
-			          />
-					<div onClick={this.stopRecording} className='AudioVis-button '>
-						<p>Stop</p>
-					</div>
+			<div style={{height: `${this.props.dialogHeight}px`}} 
+				className="messages-wrapper" >
+				<div className="messages">
+					{groups.map((group, i) =>
+						<Exchange key={i} group={group}/>
+					)}
+					<div style={{ float: "left", clear: "both" }} ref={el => this.scrollTarget = el} > </div>
 				</div>
 			</div>
+	            
 		);
 	}
 }
