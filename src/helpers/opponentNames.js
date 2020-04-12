0578471b8c38fdc35bd6fc64dfdcfdde5ee14cad
Kallirroi <kallirroi.retzepi@gmail.com>
2020-04-12 12:48:21 -0400

    struggled way more than I should have for importing a JSON file with random names, kind of hard coded it for the moment

--- src/helpers/opponentNames.js
+++ src/helpers/opponentNames.js
@@ -1,8 +1,625 @@
-const opponents = [
-	'Rando',
-	'Presidente',
-	'BlimBlimBlom'
-];
+let names = [
+  {
+      "color": "A Touch of Tan",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Across the Bay",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Adirondack",
+      "brand": "California Paints"
+  },
+  {
+      "color": "After Dinner Mint",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Almond Cookie",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Aloe Can You Go?",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Aloe Mist",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Alpine Moon",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Aluminum Blue",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Amazing Amethyst",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Amber Dawn",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Antique Rose",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Apricot Glow",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Arabian Red",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Armored Steel",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Art Deco",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Artificial Turf",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Artistic Stone",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Ashwood",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Automotive",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Autumn Bark",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Aztec Aura",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Baby Frog",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Baby Seal",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Badlands Sunset",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Baja White",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Baked Potato",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Basil Pesto",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Battle Harbor",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Bay Fog",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Beach Babies",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Beaumont Brown",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Beignet",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Below Zero",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Berry Frost",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Brushed Clay",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Bubblegum",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Caramel Apple",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Caramel Bar",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Charmed Green",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Cider Mill",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Cinnamon Stick",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Cocktail Olive",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Confident Yellow",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Coral Clay",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Coyote",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Crisp Lettuce",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Crocodile Tears",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Cultured Rose",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Dark Cavern",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Deep Carnation",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Deep Coral",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Deep Pine",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Deep Sapphire",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Encore Teal",
+      "brand": "California Paints"
+  },
+  {
+      "color": "English Holly",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Faint Clover",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Field Day",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Flash of Orange",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Fluorescent Lime",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Four Leaf Clover",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Fresh Thyme",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Friar Tuck",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Frosty Mint",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Gardening",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Garnet Evening",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Glitzy Red",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Grass Root",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Green Lane",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Green with Envy",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Herbal",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Hint of Spring",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Holly Bush",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Ice Glow",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Irish Beauty",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Irish Charm",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Jade Bracelet",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Jade Mountain",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Kale",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Magic Night",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Marble Dust",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Marshmallow Rose",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Mid-Century White",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Midland Tan",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Midnight Blush",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Mission Trail",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Mist Spirit",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Mist of Green",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Misted Eve",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Misty Hillside",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Misty Lawn",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Modern White",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Molasses",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Money",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Monsoon",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Montego Bay",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Muslin",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Natural Wool",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Navy Teal",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Neutral Era",
+      "brand": "California Paints"
+  },
+  {
+      "color": "New Wool",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Northgate Green",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Ocean Oasis",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Orange Burst",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Organic White",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Pale Frost",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Pale Morning",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Passionate Plum",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Peach",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Rare Turquoise",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Red Revival",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Refined Green",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Retro Peach",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Revival Rose",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Roman Brick",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Shaded Sun",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Shadow White",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Shamrock Field",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Sheer Lavender",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Short and Sweet",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Shrubbery",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Siamese Kitten",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Sidewalk Chalk",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Silent Ivory",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Silver Skate",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Silver Spoon",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Silver Springs",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Sky Glass",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Smart Cookie",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Smoke and Mirrors",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Snowy Shadow",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Soaring Sky",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Soft Lavender",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Soft Moss",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Soft Purple",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Sour Lemon",
+      "brand": "California Paints"
+  },
+  {
+      "color": "South Gallery",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Spacious Plain",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Spacious Sky",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Spanish White",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Sparkling Frost",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Spiced Berry",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Spring Shower",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Storm Cloud",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Strong Punch",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Succulent",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Sullen Gold",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Summerville Brown",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Tawny Amber",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Teal Waters",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Teatime Mauve",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Thinking of Hue",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Trailing Vine",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Treetop",
+      "brand": "California Paints"
+  },
+  {
+      "color": "Up North",
+      "brand": "California Paints"
+  }
+]
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -10,5 +627,6 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export const opponent = opponents[getRandomInt(0,opponents.length-1)]
+
+export const opponent = names[getRandomInt(0,names.length-1)].color
 
