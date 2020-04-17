65126a61a0a1e8b89084160ab1acc3736b3cebe3
Kallirroi <kallirroi.retzepi@gmail.com>
2020-04-17 16:03:10 -0400

    minor styling edits to About and Credits

--- src/components/main/Credits.js
+++ src/components/main/Credits.js
@@ -9,13 +9,13 @@ function Credits(props)  {
 		<div className={props.creditsClass} style={{height: `${props.dialogHeight}px`}}>
 			<div className='text' style={{fontSize: `${props.fontSize}px`}}>
 				<p>We are grateful to the Mozilla Foundation for their support of this project with a <a href="https://foundation.mozilla.org/en/awards/" rel="noopener noreferrer" target="_blank" >Creative Media Award </a> in 2019. In particular, thank you to Jenn Beard, Brett Gaylor, and the other 2019 Creative Awards Recipients. </p> 
-				<p>This project as conceived and executed by FOREIGN OBJECTS, is a design and research studio exploring the internet through the production of cultural artifacts. We build environments, tools and discourses that imagine new ways of living with the web.</p> 
-				<h4>Share </h4>
-				<span className='share'>
+				<p>This project as conceived and executed by <a href="https://www.foreignobjects.net/" rel="noopener noreferrer" target="_blank" >FOREIGN OBJECTS</a>, is a design and research studio exploring the internet through the production of cultural artifacts. We build environments, tools and discourses that imagine new ways of living with the web.</p> 
+				<h4>SHARE </h4>
+				<div className='share'>
 					<a href="https://www.foreignobjects.net/" rel="noopener noreferrer" target="_blank" > <img src={twitter} alt='twitter share' /> </a>
 					<a href="https://www.foreignobjects.net/" rel="noopener noreferrer" target="_blank" ><img src={instagram} alt='instagram share' /> </a>
 					<a href="https://www.foreignobjects.net/" rel="noopener noreferrer" target="_blank" > <img src={email} alt='email' /> </a>
-				</span>
+				</div>
 			</div>
 		</div>
 	);
