9d424991df64440fdd053b6e3fb508b595fd17b3
agnescameron <agnesfcameron@gmail.com>
2020-04-22 11:13:03 -0400

    statemap back to normal

--- src/stateMap.js
+++ src/stateMap.js
@@ -74,7 +74,7 @@ export const stateMap = [
     button2Text: '',
   },{
     step: 6, 
-    timeLimit: 5,
+    timeLimit: 45,
     messages: [],
     headerText: 'Practice round',
     main: 'Chat',
@@ -122,7 +122,7 @@ export const stateMap = [
     button2Text: '',
   },{
     step: 10, 
-    timeLimit: 5,
+    timeLimit: 45,
     messages: [],
     headerText: '',
     main: 'Chat',
@@ -194,7 +194,7 @@ export const stateMap = [
     button2Text: '',
   },{
     step: 16, 
-    timeLimit: 5,
+    timeLimit: 45,
     headerText: '',
     main: 'Chat',
     fieldTop: '',
