ed5aba8182edc5066a51981b380649a0b258a452
Kallirroi <kallirroi.retzepi@gmail.com>
2020-04-24 16:19:21 -0400

    different CSS animation

--- src/components/main/NarratorWait.js
+++ src/components/main/NarratorWait.js
@@ -3,7 +3,9 @@ import React from 'react';
 function NarratorWait(props)  {
 	return (
 		<div className={props.narratorWaitClass} style={{height: `${props.dialogHeight}px`}}>
-			<div className="text" style={{fontSize: `${props.fontSize}px`}}>{props.headline}</div>
+			<div className="text" style={{fontSize: `${props.fontSize}px`}}>
+				{props.headline}
+			</div>
 		</div>
 	);
 }
